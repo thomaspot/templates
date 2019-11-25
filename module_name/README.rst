569f4972de0a601a22ec9fa644f6cce5a8e4ee0c
Thomas Pot <thomaspot@users.noreply.github.com>
2019-11-25 10:15:34 +0100

    Add files via upload

---
+++ module_name/README.rst
@@ -0,0 +1,25 @@
+==========================
+Module Name
+==========================
+
+This module .....
+
+This module also ....
+
+**Table of contents**
+
+.. contents::
+   :local:
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/open2bizz/....>`_.
+
+
+This module is maintained by Open2bizz.
+
+.. image:: https://www.open2bizz.tech/logo.png
+   :alt: Open2Bizz
+   :target: https://www.open2bizz.tech
+
---
+++ module_name/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2020 Open2Bizz <info@open2bizz.nl>
+# License LGPL-3
+
+{
+    'name': 'module_name',
+    'summary': 'Samenvatting hier',
+    'version': '12.0.1.0.0',
+    'category': 'Catergorie',
+    'website': 'https://www.open2bizz.nl/',
+    'author': 'Open2Bizz,
+    'license': 'LGPL-3',
+    'installable': True,
+    'depends': [
+        'account',
+        'sale',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ]
